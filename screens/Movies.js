61be83d85828e8fc5632698371b0e4a84570b0ab
Aio-Dev-JS <AioDev.JS@gmail.com>
2020-09-20 00:45:07 +0900

    3.4 Api Layer

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Text, View } from 'react-native';
+import { movieApi } from '../api';
 
-export default ({ navigation }) => {
+export default () => {
+  const [movies, setMovies] = useState({
+    nowPlaying: [],
+    popular: [],
+    upcoming: [],
+    nowPlayingError: null,
+    popularError: null,
+    upcomingError: null,
+  }); // 각각 useState를 만들어주면 setState도 각각 해줘야해서 render를 여러번 하게 됨
+
+  const getData = async () => {
+    const [nowPlaying, nowPlayingError] = await movieApi.nowPlaying();
+    const [popular, popularError] = await movieApi.popular();
+    const [upcoming, upcomingError] = await movieApi.upcoming();
+    setMovies({
+      nowPlaying,
+      popular,
+      upcoming,
+      nowPlayingError,
+      popularError,
+      upcomingError,
+    });
+    // const {
+    //   data: { results },
+    // } = await movieApi.nowPlaying();
+    // console.log(results);  getAnything 함수를 만들어서 반복 최소화
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <View style={{ flex: 1, backgroundColor: 'black' }}>
-      <Text>Movies</Text>
-      <Button title="Moive" onPress={() => navigation.navigate('Detail')} />
+      <Text style={{ color: 'white' }}>{movies.nowPlaying?.length}</Text>
+      {/* <Button title="Moive" onPress={() => navigation.navigate('Detail')} /> */}
     </View>
   );
 };
--- screens/Tv.js
+++ screens/Tv.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
+import { movieApi, tvApi } from '../api';
 
 export default () => {
+  const [shows, setShows] = useState({
+    today: [],
+    thisWeek: [],
+    topRated: [],
+    popular: [],
+    todayError: null,
+    thisWeekError: null,
+    topRatedError: null,
+    popularError: null,
+  });
+
+  const getData = async () => {
+    const [today, todayError] = await tvApi.today();
+    const [thisWeek, thisWeekError] = await tvApi.thisWeek();
+    const [topRated, topRatedError] = await tvApi.topRated();
+    const [popular, popularError] = await tvApi.popular();
+    setShows({
+      today,
+      thisWeek,
+      topRated,
+      popular,
+      todayError,
+      thisWeekError,
+      topRatedError,
+      popularError,
+    });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <View>
-      <Text>TV</Text>
+      <Text>{shows.popular?.length}</Text>
     </View>
   );
 };
