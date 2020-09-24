bd2f9cb9ecb65d94353015fbd73eae29ab7ca155
Aio-Dev-JS <AioDev.JS@gmail.com>
2020-09-25 04:39:30 +0900

    4.4 add poster to slider

--- components/Movies/Slide.js
+++ components/Movies/Slide.js
@@ -3,23 +3,78 @@ import styled from 'styled-components/native';
 import PropTypes from 'prop-types';
 import { Dimensions } from 'react-native';
 import { apiImage } from '../../api';
-
-const { width: WIDTH, height: HEIGHT } = Dimensions.get('screen');
+import Poster from '../Poster';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const Container = styled.View`
-  width: ${WIDTH}px;
-  height: ${HEIGHT / 4}px;
-  background-color: red;
+  width: 100%;
+  height: 100%;
 `;
 
 const BG = styled.Image`
   width: 100%;
   height: 100%;
+  opacity: 0.4;
+  position: absolute;
 `;
 
-const Slide = ({ id, title, backgroundImage, votes, overview }) => (
+const Content = styled.View`
+  height: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+const Data = styled.View`
+  align-items: flex-start;
+  width: 50%;
+`;
+const Title = styled.Text`
+  color: white;
+  font-weight: bold;
+  font-size: 19px;
+  margin-bottom: 10px;
+`;
+const Votes = styled.Text`
+  color: white;
+  color: rgb(220, 220, 220);
+  margin-bottom: 7px;
+  font-size: 12px;
+`;
+const OverView = styled.Text`
+  color: white;
+  color: rgb(220, 220, 220);
+  font-size: 14px;
+  font-weight: 500;
+`;
+
+const Button = styled.View`
+  margin-top: 10px;
+  background-color: #e74c3c;
+  padding: 5px 10px;
+  border-radius: 5px;
+`;
+
+const ButtonText = styled.Text`
+  color: white;
+`;
+
+const Slide = ({ id, title, backgroundImage, votes, overview, poster }) => (
   <Container>
-    <BG source={{ uri: apiImage(backgroundImage) }} />
+    <BG resizeMode="cover" source={{ uri: apiImage(backgroundImage) }} />
+    <Content>
+      <Poster url={apiImage(poster)} />
+      <Data>
+        <Title>{title.slice(0, 30)}</Title>
+        <Votes>👍 {votes}/10</Votes>
+        <OverView>{overview.slice(0, 100)}</OverView>
+        <TouchableOpacity>
+          <Button>
+            <ButtonText>View Details</ButtonText>
+          </Button>
+        </TouchableOpacity>
+      </Data>
+    </Content>
   </Container>
 );
 
